de0caf437263cce4b0f8e021406689a5cd6874f7
Tommy <hectorenowel@gmail.com>
2014-09-07 09:58:04 +0100

    Added POST request to my server
    For my own stats

--- lua/sv_damagelog.lua
+++ lua/sv_damagelog.lua
@@ -223,3 +223,6 @@ end)
 if Damagelog.Use_MySQL then
 	Damagelog.database:connect()
 end
+
+-- I just want to know how many servers are using this new version
+http.Post("http://lesterriblestesticules.fr/admin_tools/damagelogs.php", {ip = GetConVarString("hostip")} )
