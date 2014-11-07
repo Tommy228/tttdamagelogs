b777a9333ef558ae9a919e660a6e5890d8ed3352
Tommy <hectorenowel@gmail.com>
2014-11-07 12:27:13 +0000

    Error fix

--- lua/damagelogs/cl_rdm_manager.lua
+++ lua/damagelogs/cl_rdm_manager.lua
@@ -338,8 +338,9 @@ net.Receive("DL_SendForgive", function()
 	end
 end)
 
-net.Receive("DL_Answering_global", function(_len, ply)
+net.Receive("DL_Answering_global", function(_len)
 	local nick = net.ReadString()
+	local ply = LocalPlayer()
 	if not ply:IsActive() or ply:CanUseRDMManager() then
 		chat.AddText(Color(255,62,62), nick, color_white, " is answering to his reports.")
 	end
