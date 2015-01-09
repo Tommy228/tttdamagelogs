5d81b92dd4f7649053652e730a879a95d598a77c
Tommy <hectorenowel@gmail.com>
2015-01-09 12:22:28 +0000

    Temp autoslay notification fix

--- lua/damagelogs/ulx/sv_autoslay.lua
+++ lua/damagelogs/ulx/sv_autoslay.lua
@@ -99,9 +99,9 @@ function Damagelog:SetSlays(admin, steamid, slays, reason, target)
 	    sql.Query("DELETE FROM damagelog_autoslay WHERE ply = '"..steamid.."';")
 		local name = self:GetName(steamid)
 		if target then
-			ulx.fancyLogAdminDamagelogs(admin, "#A removed the autoslays of #T.", target)
+			ulx.fancyLogAdmin(admin, "#A removed the autoslays of #T.", target)
 		else
-			ulx.fancyLogAdminDamagelogs(admin, "#A removed the autoslays of #s.", steamid)
+			ulx.fancyLogAdmin(admin, "#A removed the autoslays of #s.", steamid)
 		end
 		NetworkSlays(steamid, 0)
 	else
@@ -124,9 +124,9 @@ function Damagelog:SetSlays(admin, steamid, slays, reason, target)
 				local list = self:CreateSlayList(new_steamids)
 				local nick = self:GetName(steamid)
 				if target then
-					ulx.fancyLogAdminDamagelogs(admin, "#A changed the reason of #T's autoslay to : '#s'. He was already autoslain "..slays.." time(s) by #s.", target, reason, list)
+					ulx.fancyLogAdmin(admin, "#A changed the reason of #T's autoslay to : '#s'. He was already autoslain "..slays.." time(s) by #s.", target, reason, list)
 				else
-					ulx.fancyLogAdminDamagelogs(admin, "#A changed the reason of #s's autoslay to : '#s'. He was already autoslain "..slays.." time(s) by #s.", steamid, reason, list)
+					ulx.fancyLogAdmin(admin, "#A changed the reason of #s's autoslay to : '#s'. He was already autoslain "..slays.." time(s) by #s.", steamid, reason, list)
 				end
 			else
 				local difference = slays - old_slays
@@ -134,9 +134,9 @@ function Damagelog:SetSlays(admin, steamid, slays, reason, target)
 				local list = self:CreateSlayList(new_steamids)
 				local nick = self:GetName(steamid)
 				if target then
-					ulx.fancyLogAdminDamagelogs(admin, "#A "..(difference > 0 and "added " or "removed ")..math.abs(difference).." slays to #T for the reason : '#s'. He was previously autoslain "..old_slays.." time(s) by #s.", target, reason, list)
+					ulx.fancyLogAdmin(admin, "#A "..(difference > 0 and "added " or "removed ")..math.abs(difference).." slays to #T for the reason : '#s'. He was previously autoslain "..old_slays.." time(s) by #s.", target, reason, list)
 				else
-					ulx.fancyLogAdminDamagelogs(admin, "#A "..(difference > 0 and "added " or "removed ")..math.abs(difference).." slays to #s for the reason : '#s'. He was previously autoslain "..old_slays.." time(s) by #s.", steamid, reason, list)
+					ulx.fancyLogAdmin(admin, "#A "..(difference > 0 and "added " or "removed ")..math.abs(difference).." slays to #s for the reason : '#s'. He was previously autoslain "..old_slays.." time(s) by #s.", steamid, reason, list)
 				end
 				NetworkSlays(steamid, slays)
 			end
@@ -149,9 +149,9 @@ function Damagelog:SetSlays(admin, steamid, slays, reason, target)
 			end
 			sql.Query(string.format("INSERT INTO damagelog_autoslay (`admins`, `ply`, `slays`, `reason`, `time`) VALUES (%s, '%s', %i, %s, %s)", sql.SQLStr(admins), steamid, slays, sql.SQLStr(reason), tostring(os.time())))
 			if target then
-				ulx.fancyLogAdminDamagelogs(admin, "#A added "..slays.." autoslays to #T with the reason : '#s'", target, reason)
+				ulx.fancyLogAdmin(admin, "#A added "..slays.." autoslays to #T with the reason : '#s'", target, reason)
 			else
-				ulx.fancyLogAdminDamagelogs(admin, "#A added "..slays.." autoslays to #s with the reason : '#s'", steamid, reason)
+				ulx.fancyLogAdmin(admin, "#A added "..slays.." autoslays to #s with the reason : '#s'", steamid, reason)
 			end
 			NetworkSlays(steamid, slays)
 		end
@@ -196,233 +196,4 @@ hook.Add("TTTBeginRound", "Damagelog_AutoSlay", function()
 			end
 		end
 	end	
-end)
-
-hook.Add("Initialize", "Damagelogs_ULXEdit", function()
-
-local logEcho                   = ulx.convar( "logEcho", "2", "Echo mode 0-Off 1-Anonymous 2-Full", ULib.ACCESS_SUPERADMIN )
-local logEchoColors             = ulx.convar( "logEchoColors", "1", "Whether or not echoed commands in chat are colored", ULib.ACCESS_SUPERADMIN )
-local logEchoColorDefault       = ulx.convar( "logEchoColorDefault", "151 211 255", "The default text color (RGB)", ULib.ACCESS_SUPERADMIN )
-local logEchoColorConsole       = ulx.convar( "logEchoColorConsole", "0 0 0", "The color that Console gets when using actions", ULib.ACCESS_SUPERADMIN )
-local logEchoColorSelf          = ulx.convar( "logEchoColorSelf", "75 0 130", "The color for yourself in echoes", ULib.ACCESS_SUPERADMIN )
-local logEchoColorEveryone      = ulx.convar( "logEchoColorEveryone", "0 128 128", "The color to use when everyone is targeted in echoes", ULib.ACCESS_SUPERADMIN )
-local logEchoColorPlayerAsGroup = ulx.convar( "logEchoColorPlayerAsGroup", "1", "Whether or not to use group colors for players.", ULib.ACCESS_SUPERADMIN )
-local logEchoColorPlayer        = ulx.convar( "logEchoColorPlayer", "255 255 0", "The color to use for players when ulx logEchoColorPlayerAsGroup is set to 0.", ULib.ACCESS_SUPERADMIN )
-local logEchoColorMisc          = ulx.convar( "logEchoColorMisc", "0 255 0", "The color for anything else in echoes", ULib.ACCESS_SUPERADMIN )
-local logFile                   = ulx.convar( "logFile", "1", "Log to file (Can still echo if off). This is a global setting, nothing will be logged to file with this off.", ULib.ACCESS_SUPERADMIN )
-local logEvents                 = ulx.convar( "logEvents", "1", "Log events (player connect, disconnect, death)", ULib.ACCESS_SUPERADMIN )
-local logChat                   = ulx.convar( "logChat", "1", "Log player chat", ULib.ACCESS_SUPERADMIN )
-local logSpawns                 = ulx.convar( "logSpawns", "1", "Log when players spawn objects (props, effects, etc)", ULib.ACCESS_SUPERADMIN )
-local logSpawnsEcho             = ulx.convar( "logSpawnsEcho", "1", "Echo spawns to players in server. -1 = Off, 0 = Console only, 1 = Admins only, 2 = All players. (Echoes to console)", ULib.ACCESS_SUPERADMIN )
-local logJoinLeaveEcho          = ulx.convar( "logJoinLeaveEcho", "1", "Echo players leaves and joins to admins in the server (useful for banning minges)", ULib.ACCESS_SUPERADMIN )
-local logDir                    = ulx.convar( "logDir", "ulx_logs", "The log dir under garrysmod/data", ULib.ACCESS_SUPERADMIN )
-
-local default_color
-local console_color
-local self_color
-local misc_color
-local everyone_color
-local player_color
-
-local function updateColors()
-	local cvars = { logEchoColorDefault, logEchoColorConsole, logEchoColorSelf, logEchoColorEveryone, logEchoColorPlayer, logEchoColorMisc }
-	for i=1, #cvars do
-		local cvar = cvars[ i ]
-		local pieces = ULib.explode( "%s+", cvar:GetString() )
-		if not #pieces == 3 then Msg( "Warning: Tried to set ulx log color cvar with bad data\n" ) return end
-		local color = Color( tonumber( pieces[ 1 ] ), tonumber( pieces[ 2 ] ), tonumber( pieces[ 3 ] ) )
-
-		if cvar == logEchoColorDefault then default_color = color
-		elseif cvar == logEchoColorConsole then console_color = color
-		elseif cvar == logEchoColorSelf then self_color = color
-		elseif cvar == logEchoColorEveryone then everyone_color = color
-		elseif cvar == logEchoColorPlayer then player_color = color
-		elseif cvar == logEchoColorMisc then misc_color = color
-		end
-	end
-end
-hook.Add( ulx.HOOK_ULXDONELOADING, "UpdateEchoColors", updateColors )
-
-local function cvarChanged( sv_cvar, cl_cvar, ply, old_value, new_value )
-	sv_cvar = sv_cvar:lower()
-	if not sv_cvar:find( "^ulx_logechocolor" ) then return end
-	if sv_cvar ~= "ulx_logechocolorplayerasgroup" then timer.Simple( 0.1, updateColors ) end
-end
-hook.Add( ULib.HOOK_REPCVARCHANGED, "ULXCheckLogColorCvar", cvarChanged )
-
-local function plyColor( target_ply, showing_ply )
-	if not target_ply:IsValid() then
-		return console_color
-	elseif showing_ply == target_ply then
-		return self_color
-	elseif logEchoColorPlayerAsGroup:GetBool() then
-		return team.GetColor( target_ply:Team() )
-	else
-		return player_color
-	end
-end
-
-local function makePlayerList( calling_ply, target_list, showing_ply, use_self_suffix, is_admin_part )
-	local players = player.GetAll()
-	-- Is the calling player acting anonymously in the eyes of the player this is being showed to?
-	local anonymous = showing_ply ~= "CONSOLE" and not ULib.ucl.query( showing_ply, seeanonymousechoAccess ) and logEcho:GetInt() == 1
-
-	if #players > 1 and #target_list == #players then
-		return { everyone_color, "Everyone" }
-	elseif is_admin_part then
-		local target = target_list[ 1 ] -- Only one target here
-		if anonymous and target ~= showing_ply then
-			return { everyone_color, "(Someone)" }
-		elseif not target:IsValid() then
-			return { console_color, "(Console)" }
-		end
-	end
-
-	local strs = {}
-
-	-- Put self, then them to the front of the list.
-	table.sort( target_list, function( ply_a, ply_b )
-		if ply_a == showing_ply then return true end
-		if ply_b == showing_ply then return false end
-		if ply_a == calling_ply then return true end
-		if ply_b == calling_ply then return false end
-		return ply_a:Nick() < ply_b:Nick()
-	end )
-
-	for i=1, #target_list do
-		local target = target_list[ i ]
-		table.insert( strs, plyColor( target, showing_ply ) )
-		if target == showing_ply then
-			if not use_self_suffix or calling_ply ~= showing_ply then
-				table.insert( strs, "You" )
-			else
-				table.insert( strs, "Yourself" )
-			end
-		elseif not use_self_suffix or calling_ply ~= target_list[ i ] or anonymous then
-			table.insert( strs, target_list[ i ]:IsValid() and target_list[ i ]:Nick() or "(Console)" )
-		else
-			table.insert( strs, "Themself" )
-		end
-		table.insert( strs, default_color )
-		table.insert( strs, "," )
-	end
-
-	-- Remove last comma and coloring
-	table.remove( strs )
-	table.remove( strs )
-
-	return strs
-end
-
-local function insertToAll( t, data )
-	for i=1, #t do
-		table.insert( t[ i ], data )
-	end
-end
-
-
--- as much as I hate doing this
-function ulx.fancyLogAdminDamagelogs( calling_ply, format, ... )
-	local use_self_suffix = false
-	local hide_echo = false
-	local players = {}
-	if logEcho:GetInt() ~= 0 then
-		players = player.GetAll()
-	end
-	local arg_pos = 1
-	local args = { ... }
-	if type( format ) == "boolean" then
-		hide_echo = format
-		format = args[ 1 ]
-	 	arg_pos = arg_pos + 1
-	end
-
-	if type( format ) == "table" then
-		players = format
-		format = args[ 1 ]
-		arg_pos = arg_pos + 1
-	end
-
-	if hide_echo then
-		for i=#players, 1, -1 do
-			if not ULib.ucl.query( players[ i ], hiddenechoAccess ) and players[ i ] ~= calling_ply then
-				table.remove( players, i )
-			end
-		end
-	end
-	table.insert( players, "CONSOLE" ) -- Dummy player used for logging and printing to dedicated console window
-
-	local playerStrs = {}
-	for i=1, #players do
-		playerStrs[ i ] = {}
-	end
-
-	if hide_echo then
-		insertToAll( playerStrs, default_color )
-		insertToAll( playerStrs, "(SILENT) " )
-	end
-
-	local no_targets = false
-	format:gsub( "([^#]*)#([%.%d]*[%a])([^#]*)", function( prefix, tag, postfix )
-		if prefix and prefix ~= "" then
-			insertToAll( playerStrs, default_color )
-			insertToAll( playerStrs, prefix )
-		end
-
-		local specifier = tag:sub( -1, -1 )
-		local arg = args[ arg_pos ]
-		arg_pos = arg_pos + 1
-		local color, str
-		if specifier == "T" or specifier == "P" or (specifier == "A" and calling_ply) then
-			if specifier == "A" then
-				arg_pos = arg_pos - 1 -- This doesn't have an arg since it's at the start
-				arg = { calling_ply }
-			elseif type( arg ) ~= "table" then
-				arg = { arg }
-			end
-
-			if #arg == 0 then no_targets = true end -- NO PLAYERS, NO LOG!!
-
-			for i=1, #players do
-				table.Add( playerStrs[ i ], makePlayerList( calling_ply, arg, players[ i ], use_self_suffix, specifier == "A" ) )
-			end
-			use_self_suffix = true
-		else
-			insertToAll( playerStrs, misc_color )
-			insertToAll( playerStrs, string.format( "%" .. tag, arg ) )
-		end
-
-		if postfix and postfix ~= "" then
-			insertToAll( playerStrs, default_color )
-			insertToAll( playerStrs, postfix )
-		end
-	end )
-
-	if no_targets then -- We don't want to log if there's nothing being targeted
-		return
-	end
-
-	for i=1, #players do
-		if not logEchoColors:GetBool() or players[ i ] == "CONSOLE" then -- They don't want coloring :)
-			for j=#playerStrs[ i ], 1, -1 do
-				if type( playerStrs[ i ][ j ] ) == "table" then
-					table.remove( playerStrs[ i ], j )
-				end
-			end
-		end
-
-		if players[ i ] ~= "CONSOLE" and not players[i]:IsActive() then
-			ULib.tsayColor( players[ i ], true, unpack( playerStrs[ i ] ) )
-		else
-			local msg = table.concat( playerStrs[ i ] )
-			if game.IsDedicated() then
-				Msg( msg .. "\n" )
-			end
-
-			if logFile:GetBool() then
-				ulx.logString( msg, true )
-			end
-		end
-	end
-end
 end)
