df28da49036752a0232ad62415a210191a976286
André Glüpker <git@wgmd.de>
2015-02-06 22:03:01 +0100

    use math.Round instead of math.ceil in date calculation

--- lua/damagelogs/ulx/sv_autoslay.lua
+++ lua/damagelogs/ulx/sv_autoslay.lua
@@ -70,15 +70,15 @@ function Damagelog:FormatTime(t)
 	elseif t < 60 then
 		if t == 1 then return "one second" else return t.." seconds" end
 	elseif t < 3600 then
-		if math.ceil(t/60) == 1 then return "one minute" else return math.ceil(t/60).." minutes" end
+		if math.Round(t/60) == 1 then return "one minute" else return math.Round(t/60).." minutes" end
 	elseif t < 24*3600 then
-		if math.ceil(t/3600) == 1 then return "one hour" else return math.ceil(t/3600).." hours" end
+		if math.Round(t/3600) == 1 then return "one hour" else return math.Round(t/3600).." hours" end
 	elseif t < 24*3600* 7 then
-		if math.ceil(t/(24*3600)) == 1 then return "one day" else return math.ceil(t/(24*3600)).." days" end
+		if math.Round(t/(24*3600)) == 1 then return "one day" else return math.Round(t/(24*3600)).." days" end
 	elseif t < 24*3600*30 then
-		if math.ceil(t/(24*3600*7)) == 1 then return "one week" else return math.ceil(t/(24*3600*7)).." weeks" end
+		if math.Round(t/(24*3600*7)) == 1 then return "one week" else return math.Round(t/(24*3600*7)).." weeks" end
 	else
-		if math.ceil(t/(24*3600*30)) == 1 then return "one month" else return math.ceil(t/(24*3600*30)).." months" end
+		if math.Round(t/(24*3600*30)) == 1 then return "one month" else return math.Round(t/(24*3600*30)).." months" end
 	end
 end
 
